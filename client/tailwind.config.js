333ffe3ed5655e914c75a18da0411346addc4ca5
Naod <chris_naod@yahoo.com>
2023-11-06 11:58:25 +1100

    complete create listing page UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans'],
+      },
+    },
   },
   plugins: [],
 };
