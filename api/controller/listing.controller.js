395420ca8fc64f8093d571653ec2cb55336ef20a
Naod <chris_naod@yahoo.com>
2023-12-13 21:23:14 +1100

    create search API route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -63,3 +63,62 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+    const order = req.query.order || 'desc';
+    // New filters
+    const bedrooms = parseInt(req.query.bedrooms) || { $gte: 0 };
+    const bathrooms = parseInt(req.query.bathrooms) || { $gte: 0 };
+
+    // New sorting option
+    const priceSort = req.query.priceSort === 'desc' ? -1 : 1;
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      furnished,
+      parking,
+      type,
+      bedrooms,
+      bathrooms,
+    })
+      .sort({
+        [sort]: order,
+        price: priceSort,
+      })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
